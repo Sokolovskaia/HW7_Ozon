923ed7c09cc50ed38391be8d7eab168d9d58572f
Sokolovskaia <sk2m.sokolovskaya@gmail.com>
2019-04-24 23:09:30 +0300

    fixed a problem with duplicate data output

--- app/main.py
+++ app/main.py
@@ -1,9 +1,10 @@
 import waitress
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, url_for, redirect
 
-from app.ozon import create_book, add_book, search_book
+from app.ozon import create_book, add_book, search_book, create_empty_book, search_book_by_id, remove_book_by_id, update_book
 
 import os
+
 
 def start():
     app = Flask(__name__)
@@ -26,16 +27,50 @@ def start():
     def index():
         search = request.args.get('search')
         if search:
-            # TODO: почистить
             results = search_book(container, search)
             return render_template('index.html', books=results, search=search)
-
         return render_template('index.html', books=container)
+
+    @app.route('/books/<book_id>')
+    def book_details(book_id):
+        result = search_book_by_id(container, book_id)
+        return render_template('book-details.html', book=result)
+
+    @app.route('/books/<book_id>/save', methods=['POST'])
+    def book_save(book_id):
+        nonlocal container
+        title = request.form['title']
+        author = request.form['author']
+        price = request.form['price']
+        tags = request.form['tags']
+        if book_id == 'new':
+            book = create_book(title=title, author=author, price=price, tags=tags)
+            container = add_book(container, book)
+        else:
+            book = search_book_by_id(container, book_id)
+            update_book(book, title=title, author=author, price=price, tags=tags)
+            # pass  # TODO: сохранить измения
+        return redirect(url_for('book_details', book_id=book['id']))
+
+    @app.route('/books/<book_id>/edit')  # id = 0, новый объект, id <> 0 - существующий
+    def book_edit(book_id):
+        book = None
+        if book_id == 'new':
+            book = create_empty_book()
+        else:
+            book = search_book_by_id(container, book_id)
+        return render_template('book-edit.html', book=book)
+
+    @app.route('/books/<book_id>/remove', methods=['POST'])  # routing, mapping url -> конкретной функцией
+    def book_remove(book_id):
+        nonlocal container
+        container = remove_book_by_id(container, book_id)
+        return redirect(url_for('index'))
 
     if os.getenv('APP_ENV') == 'PROD' and os.getenv('PORT'):
         waitress.serve(app, port=os.getenv('PORT'))
     else:
-        app.run(port=9876, debug=True)
+        app.run(port=9875, debug=True)
 
 
 if __name__ == '__main__':
--- app/ozon.py
+++ app/ozon.py
@@ -1,5 +1,9 @@
+import uuid
+
+
 def create_book(title, author, price, tags):
     return {
+        'id': str(uuid.uuid4()),
         'title': title,
         'author': author,
         'price': price,
@@ -13,20 +17,56 @@ def add_book(container, book):
     return copy
 
 
+def create_empty_book():
+    return {
+        'id': str(uuid.uuid4()),
+        'title': '',
+        'author': '',
+        'price': '',
+        'tags': '',
+    }
+
+
+def update_book(book, title, author, price, tags):
+    book['title'] = title
+    book['author'] = author
+    book['price'] = price
+    book['tags'] = tags
+
+
 def search_book(container, title):
     title_lowercased = title.strip().lower()
     result = []
+    result_tm = []
     for book in container:
         if title_lowercased in book['title'].lower():
             result.append(book)
             continue
 
-        if title_lowercased in book['author'].lower():
+        elif title_lowercased in book['author'].lower():
             result.append(book)
             continue
 
         for tag in book['tags']:
             if title_lowercased in tag:
+                result_tm.append(book)
+        for book in result_tm:                        # удаляем дубликаты
+            if book not in result:
                 result.append(book)
-                continue
+            continue
+    return result
+
+
+def search_book_by_id(container, book_id):
+    for book in container:
+        if book['id'] == book_id:
+            return book
+    # return None
+
+
+def remove_book_by_id(container, book_id):
+    result = []
+    for book in container:  # забегая вперёд - фильтрация
+        if book['id'] != book_id:
+            result.append(book)
     return result
